547945713670b2476aabce18e126d90096672a4e
KevinTss <kevin.tassi@gmail.com>
2024-01-18 23:00:24 +0100

    Add animations

--- src/components/index-page/IndexPage.tsx
+++ src/components/index-page/IndexPage.tsx
@@ -10,6 +10,8 @@ import testimonial1ImgUrl from '@assets/testimonial-1.jpg'
 import testimonial2ImgUrl from '@assets/testimonial-2.jpg'
 import testimonial3ImgUrl from '@assets/testimonial-3.jpg'
 import { Locale } from '@types';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 import './IndexPage.styles.css'
 
@@ -21,6 +23,7 @@ const words: Record<Locale, string[]> = {
 export const IndexPage = () => {
   const t = useTranslate()
   const locale = useCurrentLocale()
+
   return (
     <>
       <section className="flex justify-center flex-grow w-full">
@@ -38,9 +41,14 @@ export const IndexPage = () => {
               {t('index.header.cta')}
             </Button>
           </div>
-          <div className="hidden lg:flex">
+          <motion.div
+            className="hidden lg:flex"
+            initial={{ opacity: 0, translateX: '100%' }}
+            animate={{ opacity: 1, translateX: 0 }}
+            transition={{ duration: 0.5 }}
+          >
             <Image src={plumber1ImgUrl} alt="Plumber" />
-          </div>
+          </motion.div>
         </div>
       </section>
 
@@ -55,66 +63,7 @@ export const IndexPage = () => {
         </div>
       </section>
 
-      <section className="flex justify-center flex-grow w-full bg-slate-50">
-        <div className="flex flex-col lg:grid lg:grid-cols-2 flex-grow py-16 px-8 max-w-screen-xl gap-10">
-          <ul className="flex flex-col gap-4 order-2 lg:order-1">
-            <li className="flex flex-col gap-2 bg-white p-4 rounded-lg shadow-sm hover:shadow-md gap-4">
-              <div className="text-blue-600 text-2xl">
-                <Icon name="thermometer" />
-              </div>
-              <div className="flex flex-col gap-5">
-                <Heading level={3} underline>{t('index.service.1.title')}</Heading>
-                <Paragraph>{t('index.service.1.description')}</Paragraph>
-              </div>
-            </li>
-            <li className="flex flex-col gap-2 bg-white p-4 rounded-lg shadow-sm hover:shadow-md gap-4">
-              <div className="text-blue-600 text-2xl">
-                <Icon name="sun" />
-              </div>
-              <div className="flex flex-col gap-5">
-                <Heading level={3} underline>{t('index.service.2.title')}</Heading>
-                <Paragraph>{t('index.service.2.description')}</Paragraph>
-              </div>
-            </li>
-            <li className="flex flex-col gap-2 bg-white p-4 rounded-lg shadow-sm hover:shadow-md gap-4">
-              <div className="text-blue-600 text-2xl">
-                <Icon name="droplet" />
-              </div>
-              <div className="flex flex-col gap-5">
-                <Heading level={3} underline>{t('index.service.3.title')}</Heading>
-                <Paragraph>{t('index.service.3.description')}</Paragraph>
-              </div>
-            </li>
-            <li className="flex flex-col gap-2 bg-white p-4 rounded-lg shadow-sm hover:shadow-md gap-4">
-              <div className="text-blue-600 text-2xl">
-                <Icon name="home" />
-              </div>
-              <div className="flex flex-col gap-5">
-                <Heading level={3} underline>{t('index.service.4.title')}</Heading>
-                <Paragraph>{t('index.service.4.description')}</Paragraph>
-              </div>
-            </li>
-            <li className="flex flex-col gap-2 bg-white p-4 rounded-lg shadow-sm hover:shadow-md gap-4">
-              <div className="text-blue-600 text-2xl">
-                <Icon name="filter" />
-              </div>
-              <div className="flex flex-col gap-5">
-                <Heading level={3} underline>{t('index.service.5.title')}</Heading>
-                <Paragraph>{t('index.service.5.description')}</Paragraph>
-              </div>
-            </li>
-          </ul>
-          <div className="order-1 lg:order-2">
-            <div className="sticky top-32 flex flex-col justify-start items-start gap-8 pl-0 log:pl-6">
-              <Badge>{t('index.service.badge')}</Badge>
-              <Heading level={2} underline>{t('index.service.title')}</Heading>
-              <Paragraph style={{ maxWidth: '460px' }}>
-                {t('index.service.description')}
-              </Paragraph>
-            </div>
-          </div>
-        </div>
-      </section>
+      <SectionServices />
 
       <section className="flex flex-col items-center flex-grow w-full py-16">
         <div
@@ -148,5 +97,107 @@ export const IndexPage = () => {
         </div>
       </section>
     </>
+  )
+}
+
+const SectionServices = () => {
+  const t = useTranslate()
+  const { ref: refLi1, inView: isLi1InView } = useInView();
+  const { ref: refLi2, inView: isLi2InView } = useInView();
+  const { ref: refLi3, inView: isLi3InView } = useInView();
+  const { ref: refLi4, inView: isLi4InView } = useInView();
+  const { ref: refLi5, inView: isLi5InView } = useInView();
+  // console.log('inView', isLi1InView)
+  return (
+    <section className="flex justify-center flex-grow w-full bg-slate-50">
+      <div className="flex flex-col lg:grid lg:grid-cols-2 flex-grow py-16 px-8 max-w-screen-xl gap-10">
+        <ul className="flex flex-col gap-4 order-2 lg:order-1">
+          <motion.li
+            ref={refLi1}
+            className="flex flex-col gap-2 bg-white p-4 rounded-lg shadow-sm hover:shadow-md gap-4"
+            initial={{ opacity: 0, translateX: '-100%' }}
+            animate={{ opacity: isLi1InView ? 1 : 0, translateX: isLi1InView ? 0 : '-100%' }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="text-blue-600 text-2xl">
+              <Icon name="thermometer" />
+            </div>
+            <div className="flex flex-col gap-5">
+              <Heading level={3} underline>{t('index.service.1.title')}</Heading>
+              <Paragraph>{t('index.service.1.description')}</Paragraph>
+            </div>
+          </motion.li>
+          <motion.li
+            ref={refLi2}
+            className="flex flex-col gap-2 bg-white p-4 rounded-lg shadow-sm hover:shadow-md gap-4"
+            initial={{ opacity: 0, translateX: '-100%' }}
+            animate={{ opacity: isLi2InView ? 1 : 0, translateX: isLi2InView ? 0 : '-100%' }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="text-blue-600 text-2xl">
+              <Icon name="sun" />
+            </div>
+            <div className="flex flex-col gap-5">
+              <Heading level={3} underline>{t('index.service.2.title')}</Heading>
+              <Paragraph>{t('index.service.2.description')}</Paragraph>
+            </div>
+          </motion.li>
+          <motion.li
+            ref={refLi3}
+            className="flex flex-col gap-2 bg-white p-4 rounded-lg shadow-sm hover:shadow-md gap-4"
+            initial={{ opacity: 0, translateX: '-100%' }}
+            animate={{ opacity: isLi3InView ? 1 : 0, translateX: isLi3InView ? 0 : '-100%' }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="text-blue-600 text-2xl">
+              <Icon name="droplet" />
+            </div>
+            <div className="flex flex-col gap-5">
+              <Heading level={3} underline>{t('index.service.3.title')}</Heading>
+              <Paragraph>{t('index.service.3.description')}</Paragraph>
+            </div>
+          </motion.li>
+          <motion.li
+            ref={refLi4}
+            className="flex flex-col gap-2 bg-white p-4 rounded-lg shadow-sm hover:shadow-md gap-4"
+            initial={{ opacity: 0, translateX: '-100%' }}
+            animate={{ opacity: isLi4InView ? 1 : 0, translateX: isLi4InView ? 0 : '-100%' }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="text-blue-600 text-2xl">
+              <Icon name="home" />
+            </div>
+            <div className="flex flex-col gap-5">
+              <Heading level={3} underline>{t('index.service.4.title')}</Heading>
+              <Paragraph>{t('index.service.4.description')}</Paragraph>
+            </div>
+          </motion.li>
+          <motion.li
+            ref={refLi5}
+            className="flex flex-col gap-2 bg-white p-4 rounded-lg shadow-sm hover:shadow-md gap-4"
+            initial={{ opacity: 0, translateX: '-100%' }}
+            animate={{ opacity: isLi5InView ? 1 : 0, translateX: isLi5InView ? 0 : '-100%' }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="text-blue-600 text-2xl">
+              <Icon name="filter" />
+            </div>
+            <div className="flex flex-col gap-5">
+              <Heading level={3} underline>{t('index.service.5.title')}</Heading>
+              <Paragraph>{t('index.service.5.description')}</Paragraph>
+            </div>
+          </motion.li>
+        </ul>
+        <div className="order-1 lg:order-2">
+          <div className="sticky top-32 flex flex-col justify-start items-start gap-8 pl-0 log:pl-6">
+            <Badge>{t('index.service.badge')}</Badge>
+            <Heading level={2} underline>{t('index.service.title')}</Heading>
+            <Paragraph style={{ maxWidth: '460px' }}>
+              {t('index.service.description')}
+            </Paragraph>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
