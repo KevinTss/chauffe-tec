5b386bc0686e0380fe204c77b12942919d00e84c
KevinTss <kevin.tassi@gmail.com>
2024-01-18 22:42:39 +0100

    Add animation to nav mobile

--- src/components/mobile-navigation/MobileNavigation.tsx
+++ src/components/mobile-navigation/MobileNavigation.tsx
@@ -6,6 +6,7 @@ import { DictionaryKeys } from '@types'
 import { useCurrentLocale, useTranslate } from '@hooks'
 import { LocaleSwitcher, Icon } from '@components'
 import { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
 
 const navigationLinks = [
   { trKey: 'nav.index', path: pathnames['index'] },
@@ -22,32 +23,39 @@ export const MobileNavigation = () => {
       <button className='block lg:hidden pointer text-white' onClick={() => setIsOpen(true)}>
         <Icon name='menu' />
       </button>
-      <div
-        className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 ${isOpen ? '' : 'hidden'}`}
-      // onClick={() => setIsOpen(false)}
-      >
-        <div className="flex items-center justify-center h-full">
-          <div className="flex flex-col bg-white w-full h-full">
-            <button
-              className="mt-8 mr-10 mb-8 self-end w-4 h-4"
-              onClick={() => setIsOpen(false)}
-            >
-              {/* @ts-ignore */}
-              <Icon name='x' style={{ width: '24px', height: '24px' }} />
-            </button>
-            {navigationLinks.map(({ trKey, path }) => (
-              <Link
-                key={trKey}
-                className="text-base hover:bg-blue-300 px-6 py-2 rounded-xl cursor-pointer"
-                href={path[locale]}
-              >
-                {t(trKey)}
-              </Link>
-            ))}
-            <LocaleSwitcher />
-          </div>
-        </div>
-      </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            className={`fixed top-0 left-0 w-full h-full bg-black`}
+            initial={{ opacity: 0, translateX: '100%' }}
+            animate={{ opacity: 1, translateX: 0 }}
+            exit={{ opacity: 0, translateX: '100%' }}
+            transition={{ duration: 0.2 }}
+          >
+            <div className="flex items-center justify-center h-full">
+              <div className="flex flex-col bg-white w-full h-full">
+                <button
+                  className="mt-8 mr-10 mb-8 self-end w-4 h-4"
+                  onClick={() => setIsOpen(false)}
+                >
+                  {/* @ts-ignore */}
+                  <Icon name='x' style={{ width: '24px', height: '24px' }} />
+                </button>
+                {navigationLinks.map(({ trKey, path }) => (
+                  <Link
+                    key={trKey}
+                    className="text-base hover:bg-blue-300 px-6 py-2 rounded-xl cursor-pointer"
+                    href={path[locale]}
+                  >
+                    {t(trKey)}
+                  </Link>
+                ))}
+                <LocaleSwitcher />
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   )
 }
